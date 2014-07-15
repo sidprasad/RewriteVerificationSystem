accdfbe55195e04f2e2a950e6271c97fb18d13c3
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2014-07-15 09:35:37 -0400

    Added some example Certificates

--- eq_checker/operators_to_pairs.pl
+++
@@ -1,8 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Replaces all t(OP, L) with t((OP,M), L)
-%treeify_pairs(t(K,[]), t(K,[]), _).
-%treeify_pairs(t(K, L), t((K, M), Lnew), M) :- treeify_subterms(L, Lnew, [], M).
-%treeify_subterms(L, Lnew, Lprev, M) :- L = [X|Tail],
-%										treeify_pairs(X, X_, M),
-%										treeify_subterms(Tail, Lnew, [X_|Lprev], M).
-%treeify_subterms([], Y, X, _):- reverse(X, Y).		
